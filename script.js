75125fa8bbcfc6aa1f1df582bff52e208f13c7a9
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-21 14:27:10 -0300

    entrega requisito 6

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.onload = function jogoDaCor() {
         let r = parseInt(Math.random() * 255);
         let g = parseInt(Math.random() * 255);
         let b = parseInt(Math.random() * 255);
-
+        /* conteudo pesquisado ein: https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript#:~:text=Gerando%20cores%20RGBA&text=A%20fun%C3%A7%C3%A3o%20Math.,ser%C3%A1%20a%20opacidade%20da%20cor. */
         if (cores[i].style.backgroundColor !== rgbCerto) {
             cores[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
         }
@@ -40,11 +40,3 @@ window.onload = function jogoDaCor() {
         jogoDaCor();
     });
 }
-
-
-
-
-/* conteudo pesquisado ein: https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript#:~:text=Gerando%20cores%20RGBA&text=A%20fun%C3%A7%C3%A3o%20Math.,ser%C3%A1%20a%20opacidade%20da%20cor. */
-
-    //escolendo a cor
-
